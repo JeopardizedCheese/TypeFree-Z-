d8076c127bbc32f7a525a648c799093abda09936
JeopardizedCheese <pon70454@gmail.com>
2025-05-02 20:04:31 +0700

    updated landing page and responsiveness

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -1,16 +1,19 @@
 <script>
     let {img="#", name="ชื่อแร่", ds="des" }=$props()
 </script>
-<div
-    class="box bg-gray-600 w-[300px]  p-10 m-10 rounded-4xl">
-    <img src={img} alt="IMG" class="" />
-    <p class="text-white text-2xl">{name}</p>
-    <p class="text-xl text-amber-200">TextFree(z)</p>
-    <p class="text-white text-xl pb-2 ">
+
+<div class="flex flex-col items-center bg-gray-900 w-64 p-6 rounded-2xl border-2 border-amber-200/3 shadow-lg transform transition-all duration-300 hover:scale-105 hover:border-amber-200">
+
+    <img src={img} alt="IMG" class="w-28 h-28 object-contain mb-4" />
+    <h3 class="text-white text-2xl font-bold mb-1">{name}</h3>
+    <p class="text-lg text-amber-200 mb-4">TextFree(z)</p>
+
+    <p class="text-white text-center mb-6 overflow-y-auto h-24">
         {ds}
     </p>
-    <a href="/pages">
-     <button class="bg-white text-emerald-700 text-xl rounded-4xl h-16 p-2.5 hover:bg-amber-200 ">ออกตามหาอัญมณีเลย!</button>
+
+    <a href="/pages" class="w-full block">
+     <button class="w-full bg-white text-emerald-700 font-bold text-lg rounded-xl py-3 transition-all duration-300 hover:bg-amber-200">ออกตามหาอัญมณีเลย!</button>
     </a>
     
 </div>
--- src/routes/pages/+page.svelte
+++ src/routes/pages/+page.svelte
@@ -335,7 +335,7 @@
         {#if !isActive && !endTime}
             <button
                 on:click={startGame}
-                class="px-6 py-2 bg-[#6DE1D2] text-blue-950 font-bold rounded-md hover:bg-[#5bc7ba] transition-colors"
+                class="btn btn-accent text-black"
             >
                 Start
             </button>
